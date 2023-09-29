363eb9bc5c3a7ed828b4863f9f91c5f1c87beae9
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-09-29 16:01:00 +0100

    task2-3

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,7 +9,8 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t tmp, i, j;
+	size_t i, j, i_idx, j_idx;
+	int tmp;
 
 	i = 0;
 	while (i < size)
@@ -17,15 +18,18 @@ void selection_sort(int *array, size_t size)
 		j = i;
 		while (j < size)
 		{
-			if (array[i] > array[j])
+			tmp = array[i];
+			if (tmp > array[j])
 			{
-				tmp = array[i];
-				array[i] = array[j];
-				array[j] = tmp;
+				tmp = array[j];
+				i_idx = i;
+				j_idx = j;
 			}
 			j++;
 		}
 		i++;
+		array[j_idx] = array[i_idx];
+		array[i_idx] = tmp;
 		print_array(array, size);
 	}
 }
